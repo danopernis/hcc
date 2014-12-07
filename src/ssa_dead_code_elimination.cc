aa47621d786e88f9f26fb3484ca68d852d09c1e4
Dano Pernis <danopernis@gmail.com>
2014-12-07 18:26:23 +0100

    Simplified worklist of dead_code_elimination()

--- src/ssa_dead_code_elimination.cc
+++ src/ssa_dead_code_elimination.cc
@@ -10,50 +10,46 @@ void subroutine::dead_code_elimination()
 {
     recompute_control_flow_graph();
 
-    std::stack<std::pair<instruction_list::iterator, int>> worklist; // instruction and its block
-    for_each_bb([&] (basic_block& block) {
-        for (auto i = block.begin(), e = block.end(); i != e; ++i) {
-            switch (i->type) {
-            case instruction_type::JUMP:
-            case instruction_type::BRANCH:
-            case instruction_type::CALL:
-            case instruction_type::RETURN:
-            case instruction_type::LOAD:
-            case instruction_type::STORE:
-                i->mark = true;
-                worklist.emplace(i, block.index);
-                break;
-            default:
-                i->mark = false;
-            }
+    std::stack<instruction_list::iterator> worklist;
+    for (auto i = instructions.begin(), e = instructions.end(); i != e; ++i) {
+        switch (i->type) {
+        case instruction_type::JUMP:
+        case instruction_type::BRANCH:
+        case instruction_type::CALL:
+        case instruction_type::RETURN:
+        case instruction_type::LOAD:
+        case instruction_type::STORE:
+            i->mark = true;
+            worklist.emplace(i);
+            break;
+        default:
+            i->mark = false;
         }
-    });
+    }
 
     while (!worklist.empty()) {
         auto w = worklist.top();
         worklist.pop();
 
         // for each use...
-        w.first->use_apply([&] (std::string& use) {
+        w->use_apply([&] (std::string& use) {
             // ...find the definer...
-            for_each_bb([&] (basic_block& block) {
-                for (auto i = block.begin(), e = block.end(); i != e; ++i) {
-                    i->def_apply([&] (std::string& def) {
-                        if (def == use && !i->mark) {
-                            // ... mark and append to worklist
-                            i->mark = true;;
-                            worklist.emplace(i, block.index);
-                        }
-                    });
-                }
-            });
+            for (auto i = instructions.begin(), e = instructions.end(); i != e; ++i) {
+                i->def_apply([&] (std::string& def) {
+                    if (def == use && !i->mark) {
+                        // ... mark and append to worklist
+                        i->mark = true;
+                        worklist.emplace(i);
+                    }
+                });
+            }
         });
 
-        for_each_reverse_dfs(w.second, [&] (basic_block& block) {
+        for_each_reverse_dfs(w->basic_block, [&] (basic_block& block) {
             auto i = block.end();
             if (!i->mark) {
                 i->mark = true;
-                worklist.emplace(i, block.index);
+                worklist.emplace(i);
             }
         });
     }
