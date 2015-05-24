a1b915f6a871d9db486c06652f6012f28b68501e
Dano Pernis <danopernis@gmail.com>
2015-05-24 18:12:35 +0200

    Refactored SSA dead code elimination.
    Extracts multiple instruction from worklist at once.
    Does not need iterators in marking phase of the algorithm.

--- src/ssa_dead_code_elimination.cc
+++ src/ssa_dead_code_elimination.cc
@@ -10,77 +10,96 @@ void subroutine::dead_code_elimination()
 {
     recompute_dominance();
 
-    std::stack<instruction_list::iterator> worklist;
+    // Dead code elimination (DCE) eliminates all non-critical instructions.
+    // Critical instruction is any instruction capable of changing observable
+    // behavior of given subroutine. DCE works by first marking the implicitly
+    // critical instructions and then iteratively broadening a set of critical
+    // instructions until no more instructions can be added. To this end,
+    // instrutions are pushed into and pulled from a worklist. As soon as the
+    // marking algorithm converges, subsequest pass sweeps all non-critical
+    // instructions and DCE ends.
+
+    // current iteration of the algorithm
+    auto work = 1;
+
+    // first iteration
     for_each_bb([&] (basic_block& bb) {
-    for (auto i = bb.instructions.begin(), e = bb.instructions.end(); i != e; ++i) {
-        i->basic_block = bb.name;
-        switch (i->type) {
-        case instruction_type::JUMP:
-        case instruction_type::JLT:
-        case instruction_type::JEQ:
-        case instruction_type::CALL:
-        case instruction_type::RETURN:
-        case instruction_type::LOAD:
-        case instruction_type::STORE:
-            i->mark = true;
-            worklist.emplace(i);
-            break;
-        default:
-            i->mark = false;
+        for (auto& i : bb.instructions) {
+            switch (i.type) {
+            case instruction_type::JUMP:
+            case instruction_type::CALL:
+            case instruction_type::RETURN:
+            case instruction_type::STORE:
+                i.work = work;
+                break;
+            default:
+                i.work = 0;
+            }
         }
-    }
     });
 
-    while (!worklist.empty()) {
-        auto w = worklist.top();
-        worklist.pop();
+    // iterate until marking converges
+    bool changed = true;
+    while (changed) {
+        changed = false;
 
-        // for each use...
-        w->use_apply([&] (argument& a_use) {
-            if (!a_use.is_reg()) {
-                return;
-            }
-            const auto& use = a_use.get_reg();
-            // ...find the definer...
-            for_each_bb([&] (basic_block& bb) {
-            for (auto i = bb.instructions.begin(), e = bb.instructions.end(); i != e; ++i) {
-                i->def_apply([&] (argument& a_def) {
-                    if (!a_def.is_reg()) {
-                        return;
-                    }
-                    const auto& def = a_def.get_reg();
+        std::set<reg> uses;
+        std::set<label> bbs;
 
-                    if (def == use && !i->mark) {
-                        // ... mark and append to worklist
-                        i->mark = true;
-                        worklist.emplace(i);
-                    }
-                });
+        for_each_bb([&] (basic_block& bb) {
+            for (auto& i : bb.instructions) {
+                // if the instruction is on the worklist, collect dependencies
+                if (i.work == work) {
+                    // collect registers
+                    i.use_apply([&] (argument& a) {
+                        if (a.is_reg()) {
+                            uses.emplace(a.get_reg());
+                        }
+                    });
+                    // collect basic blocks
+                    for_each_reverse_dfs(bb.name, [&] (basic_block& block) {
+                        bbs.emplace(block.name);
+                    });
+                }
             }
-            });
         });
 
-        for_each_reverse_dfs(w->basic_block, [&] (basic_block& block) {
+        // next iteration
+        ++work;
+
+        for_each_bb([&] (basic_block& block) {
             if (block.instructions.empty()) {
                 return;
             }
+
+            // put terminator of collected basic block on the worklist
             auto i = --block.instructions.end();
-            if (!i->mark) {
-                i->mark = true;
-                worklist.emplace(i);
+            if (bbs.count(block.name) && i->work == 0) {
+                i->work = work;
+                changed = true;
+            }
+
+            // put definition of collected register on the worklist
+            for (auto& i : block.instructions) {
+                i.def_apply([&] (argument& def) {
+                    if (uses.count(def.get_reg()) && i.work == 0) {
+                        i.work = work;
+                        changed = true;
+                    }
+                });
             }
         });
     }
 
     // sweep
     for_each_bb([&] (basic_block& bb) {
-    for (auto i = bb.instructions.begin(), e = bb.instructions.end(); i != e;) {
-        if (!i->mark) {
-            i = bb.instructions.erase(i);
-        } else {
-            ++i;
+        for (auto i = bb.instructions.begin(), e = bb.instructions.end(); i != e;) {
+            if (i->work == 0) {
+                i = bb.instructions.erase(i);
+            } else {
+                ++i;
+            }
         }
-    }
     });
 }
 
