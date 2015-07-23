39404b9e8a422acddc48eab96456d8bd32678f2c
Dano Pernis <danopernis@gmail.com>
2015-07-23 23:28:29 +0200

    Added tool for generating include graph

---
+++ src/generate_include_graph.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+(
+echo "digraph depend {"
+for file1 in *.{cc,h}; do
+    if `echo -n $file1 | grep -q '\.cc$'`; then
+        echo "\"$file1\" [style=filled, fillcolor=gray];"
+    fi
+    for file2 in `grep '^#include "' $file1 |cut -d '"' -f 2`; do
+        echo "\"$file1\" -> \"$file2\";"
+    done
+done
+echo "}"
+) | dot -Tpng >include_graph.png
