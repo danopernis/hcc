8bfe79196ae96aa4c2c2e477a06cc24fefa46a95
Dano Pernis <danopernis@gmail.com>
2015-08-01 06:11:49 +0200

    Added getopt for hcc

--- src/hcc.cc
+++ src/hcc.cc
@@ -14,17 +14,77 @@
 using namespace hcc::jack;
 using namespace hcc::ssa;
 
+struct command_line_options {
+    command_line_options(int argc, char* argv[])
+    {
+        opterr = 0;
+        int opt = -1;
+        while ((opt = getopt(argc, argv, ":ho:S")) != -1) {
+            switch (opt) {
+            case 'h':
+                help = true;
+                break;
+            case 'o':
+                output = optarg;
+                break;
+            case 'S':
+                assemble = false;
+                break;
+            case '?':
+                throw std::runtime_error(
+                    std::string("Unknown command line option: ") +
+                    static_cast<char>(optopt));
+            case ':':
+                throw std::runtime_error(
+                    std::string("Missing argument for command line option: ") +
+                    static_cast<char>(optopt));
+            }
+        }
+        while (optind < argc) {
+            input_files.emplace_back(argv[optind++]);
+        }
+        if (input_files.empty() && !help) {
+            throw std::runtime_error("Missing input file(s)");
+        }
+        if (output.empty()) {
+            if (assemble) {
+                output = "output.hack";
+            } else {
+                output = "output.asm";
+            }
+        }
+    }
+
+    void print_help() const
+    {
+        std::cout <<
+            "Usage: hcc [options] file...\n"
+            "Options:\n"
+            "  -h                   Display this information\n"
+            "  -o <file>            Place the output into <file>\n"
+            "  -S                   Compile only; do not assemble\n"
+            ;
+    }
+
+    bool help {false};
+    bool assemble {true};
+    std::string output;
+    std::vector<std::string> input_files;
+};
+
 int main(int argc, char* argv[])
 try {
-    if (argc < 2) {
-        throw std::runtime_error("Missing input file(s)\n");
+    const command_line_options options {argc, argv};
+    if (options.help) {
+        options.print_help();
+        return 0;
     }
 
     // parse input
     std::vector<ast::Class> classes;
     try {
-        for (int i = 1; i<argc; ++i) {
-            std::ifstream input(argv[i]);
+        for (const auto& input_file : options.input_files) {
+            std::ifstream input {input_file};
             tokenizer t {
                 std::istreambuf_iterator<char>(input),
                 std::istreambuf_iterator<char>()};
@@ -33,7 +93,7 @@ try {
     } catch (const parse_error& e) {
         std::stringstream ss;
         ss  << "Parse error: " << e.what()
-            << " at " << e.line << ":" << e.column << '\n';
+            << " at " << e.line << ":" << e.column;
         throw std::runtime_error(ss.str());
     }
 
@@ -56,13 +116,19 @@ try {
     hcc::asm_program out;
     u.translate_to_asm(out);
     out.local_optimization();
-    out.save("output.asm");
+
+    // output
+    if (options.assemble) {
+        hcc::saveHACK(options.output, out.assemble());
+    } else {
+        out.save(options.output);
+    }
 
     return 0;
 } catch (const std::runtime_error& e) {
     std::cerr
         << "When executing "
         << boost::algorithm::join(std::vector<std::string>(argv, argv + argc), " ")
-        << " ...\n" << e.what();
+        << " ...\n" << e.what() << "\n";
     return 1;
 }
