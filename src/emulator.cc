6ffbc089d2cbff3c80356ab2412c4fdd9af0e03e
Dano Pernis <danopernis@gmail.com>
2015-08-06 22:45:28 +0200

    Included ROM.h into emulator.cc

--- src/emulator.cc
+++ src/emulator.cc
@@ -3,11 +3,68 @@
 
 #include <gtk/gtk.h>
 #include <glib.h>
+#include <fstream>
 #include <iostream>
 #include <stdexcept>
 #include <cassert>
 #include "CPU.h"
-#include "ROM.h"
+
+namespace hcc {
+
+struct ROM : public IROM {
+	unsigned short *data;
+	static const unsigned int size = 0x8000;
+	ROM() {
+		data = new unsigned short[size];
+	}
+	virtual ~ROM() {
+		delete[] data;
+	}
+
+	bool load(const char *filename) {
+		std::ifstream input(filename);
+		std::string line;
+		unsigned int counter = 0;
+		while (input.good() && counter < size) {
+			getline(input, line);
+			if (line.size() == 0)
+				continue;
+			if (line.size() != 16)
+				return false;
+
+			unsigned int instruction = 0;
+			for (unsigned int i = 0; i<16; ++i) {
+				instruction <<= 1;
+				switch (line[i]) {
+				case '0':
+					break;
+				case '1':
+					instruction |= 1;
+					break;
+				default:
+					return false;
+				}
+			}
+			data[counter++] = instruction;
+		}
+		// clear the rest
+		while (counter < size) {
+			data[counter++] = 0;
+		}
+
+		return true;
+	}
+	virtual unsigned short get(unsigned int address) const {
+		if (address < size) {
+			return data[address];
+		} else {
+			std::cerr << "requested memory at " << address << '\n';
+			throw std::runtime_error("Memory::get");
+		}
+	}
+};
+
+} // end namespace
 
 struct GUIEmulatorRAM : public hcc::IRAM {
 	static const unsigned int CHANNELS = 3;
