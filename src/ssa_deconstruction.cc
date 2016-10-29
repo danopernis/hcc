cf88518a165515383c4c56a2204792a63ceb6897
Dano Pernis <danopernis@gmail.com>
2016-10-30 01:44:54 +0200

    Optimize interference computation

--- src/ssa_deconstruction.cc
+++ src/ssa_deconstruction.cc
@@ -80,20 +80,27 @@ void naive_copy_insertion(subroutine& s, congruence_classes& cc)
     });
 }
 
-// live(x) ... set of statements were x is live
+// Determine if two registers *a* and *b* interfere in subroutine *s*
+//
+// Interfering registers have intersecting live ranges and different values:
+//
+// live(x) ... set of statements where x is live
 // live(a) intersects live(b) ... def(a) in live(b) or def(b) in live(a)
 // V(x) ... V(b) = V(a) if b <- a
 //          V(b) = b    otherwise
 // a interfere b ... live(a) intersects live(b) and V(a) != V(b)
-bool interfere(const argument& a, const argument& b, subroutine& s)
+bool interfere(const reg& a, const reg& b, subroutine& s)
 {
-    assert(a.is_reg() && b.is_reg());
-
     bool def_a_in_live_b = false;
     bool def_b_in_live_a = false;
     bool live_a = false;
     bool live_b = false;
-    std::map<std::string, std::string> V; // value of variable
+
+    // At this point, code is still in SSA, so value of each register
+    // is determined solely by its defining instruction. What is more,
+    // we can use pointer here because nothing gets reallocated.
+    // TODO use const *s* to prove that pointer usage is indeed safe.
+    std::map<reg, const instruction*> value;
 
     // TODO investigate if domtree is really needed
     // TODO live ranges are probably overestimated at the end
@@ -102,7 +109,8 @@ bool interfere(const argument& a, const argument& b, subroutine& s)
         for (auto& instr : block.instructions) {
             // live ranges
             instr.def_apply([&](argument& def) {
-                if (def == a) {
+                assert (def.is_reg());
+                if (def.get_reg() == a) {
                     live_a = true;
                     if (live_b)
                         def_a_in_live_b = true;
@@ -115,23 +123,23 @@ bool interfere(const argument& a, const argument& b, subroutine& s)
 
             // values
             if (instr.type == instruction_type::MOV) {
-                auto s0 = instr.arguments[0].save_fast();
-                auto s1 = instr.arguments[1].save_fast();
-                V[std::move(s0)] = V[std::move(s1)];
+                if (instr.arguments[1].is_reg()) {
+                    value.emplace(instr.arguments[0].get_reg(),
+                        value.at(instr.arguments[1].get_reg()));
+                } else {
+                    value.emplace(instr.arguments[0].get_reg(), &instr);
+                }
             } else {
                 instr.def_apply([&](argument& arg) {
-                    auto s0 = arg.save_fast();
-                    auto s1 = instr.save_fast();
-                    V[std::move(s0)] = std::move(s1);
+                    assert (arg.is_reg());
+                    value.emplace(arg.get_reg(), &instr);
                 });
             }
         }
     });
 
     const bool intersect = def_a_in_live_b || def_b_in_live_a;
-    auto sa = a.save_fast();
-    auto sb = b.save_fast();
-    const bool differ_in_value = V.at(sa) != V.at(sb);
+    const bool differ_in_value = value.at(a) != value.at(b);
     return intersect && differ_in_value;
 }
 
@@ -158,7 +166,7 @@ void subroutine::ssa_deconstruct()
                     continue;
 
                 if (src.is_reg() && dest.is_reg()) {
-                    if (!interfere(src, dest, *this)) {
+                    if (!interfere(src.get_reg(), dest.get_reg(), *this)) {
                         if (!has_src_class && !has_dest_class) {
                             const auto r = create_reg();
                             add_debug(r, "congruence_class");
